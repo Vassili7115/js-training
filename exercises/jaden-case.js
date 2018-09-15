08ea0e33f94720eeb6466d914a7d8fbc7caea465
Matthieu Ranucci <matthieu@MacBook-Pro-de-Matthieu.local>
2018-09-15 14:42:21 +0200

    TDD 4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,11 +9,24 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
-
-
+function jadenCase(str){
+    let arr = str.split(' ');
+    for (let i = 0; i < arr.length - 1; i++){
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1).toLowerCase();
+    }
+    return arr.join(' ');
+     }
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(typeof jadenCase, 'function')
+
+assert.strictEqual(jadenCase.length, 1)
+assert.strictEqual(jadenCase("attention DANGER ?"), "Attention Danger ?")
+assert.strictEqual(jadenCase("AttenTION danger ?"), "Attention Danger ?")
+assert.strictEqual(jadenCase("ATTENTION danger ?"), "Attention Danger ?")
+assert.strictEqual(jadenCase("attention danger ?"), "Attention Danger ?")
+
+
 // End of tests */
